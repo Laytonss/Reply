e2b75110c9694d524f0b0478115b59419d192240
Laytonss <jiajun.huang@thoughworks.com>
2024-04-15 10:26:48 +0800

    feat: add implements for bottom bar

--- app/src/main/java/com/thoughtworks/training/reply/ui/MainActivity.kt
+++ app/src/main/java/com/thoughtworks/training/reply/ui/MainActivity.kt
@@ -5,7 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.thoughtworks.training.reply.ui.components.MyApp
 import com.thoughtworks.training.reply.ui.theme.ContrastAwareReplyTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,8 +24,11 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             ContrastAwareReplyTheme {
-
+                MyApp()
             }
         }
     }
 }
+
+
+
---
+++ app/src/main/java/com/thoughtworks/training/reply/ui/components/MyAppCompose.kt
@@ -0,0 +1,69 @@
+package com.thoughtworks.training.reply.ui.components
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Image
+import androidx.compose.material.icons.filled.Mic
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+
+enum class BottomNavigationItem {
+    EMAILS, ARTICLE, CHAT, GROUP
+}
+
+@Composable
+fun MyApp() {
+    val (selectedItem, setSelectedItem) = remember { mutableStateOf(BottomNavigationItem.EMAILS) }
+    Scaffold(
+        bottomBar = {
+            AppBottomBar(selectedItem) { newItem -> setSelectedItem(newItem) }
+        }
+    ) { innerPadding ->
+        when (selectedItem) {
+            BottomNavigationItem.EMAILS -> {
+                Text(text = "EMAILS Page", modifier = Modifier.padding(innerPadding))
+            }
+
+            BottomNavigationItem.ARTICLE -> {
+                Text(text = "ARTICLE Page", modifier = Modifier.padding(innerPadding))
+            }
+
+            BottomNavigationItem.CHAT -> {
+                Text(text = "CHAT Page", modifier = Modifier.padding(innerPadding))
+            }
+
+            BottomNavigationItem.GROUP -> {
+                Text(text = "GROUP Page", modifier = Modifier.padding(innerPadding))
+            }
+        }
+    }
+}
+
+@Composable
+fun AppBottomBar(selectedItem: BottomNavigationItem, onItemSelected: (BottomNavigationItem) -> Unit) {
+    BottomAppBar(
+        actions = {
+            IconButton(onClick = { onItemSelected(BottomNavigationItem.EMAILS) }, modifier = Modifier.weight(1f)) {
+                Icon(Icons.Filled.Check, contentDescription = "Localized description")
+            }
+            IconButton(onClick = { onItemSelected(BottomNavigationItem.ARTICLE) }, modifier = Modifier.weight(1f)) {
+                Icon(Icons.Filled.Edit, contentDescription = "Localized description")
+            }
+            IconButton(onClick = { onItemSelected(BottomNavigationItem.CHAT) }, modifier = Modifier.weight(1f)) {
+                Icon(Icons.Filled.Mic, contentDescription = "Localized description")
+            }
+            IconButton(onClick = { onItemSelected(BottomNavigationItem.GROUP) }, modifier = Modifier.weight(1f)) {
+                Icon(Icons.Filled.Image, contentDescription = "Localized description")
+            }
+        },
+    )
+}
