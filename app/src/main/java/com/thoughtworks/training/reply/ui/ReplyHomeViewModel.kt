050500faacf937b2e05b0273322ec43a3f9e055c
Laytonss <jiajun.huang@thoughworks.com>
2024-04-15 08:56:06 +0800

    feat: add flow data in ReplyHomeViewModel

--- app/src/main/java/com/thoughtworks/training/reply/ui/ReplyHomeViewModel.kt
+++ app/src/main/java/com/thoughtworks/training/reply/ui/ReplyHomeViewModel.kt
@@ -1,15 +1,20 @@
 package com.thoughtworks.training.reply.ui
 
 import androidx.lifecycle.ViewModel
-import com.thoughtworks.training.reply.data.Email
+import com.thoughtworks.training.reply.data.AccountsRepository
+import com.thoughtworks.training.reply.data.AccountsRepositoryImpl
 import com.thoughtworks.training.reply.data.EmailsRepository
 import com.thoughtworks.training.reply.data.EmailsRepositoryImpl
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+
 
 class ReplyHomeViewModel(
-    private val emailsRepository: EmailsRepository = EmailsRepositoryImpl()
+    private val emailsRepository: EmailsRepository = EmailsRepositoryImpl(),
+    private val accountsRepository: AccountsRepository = AccountsRepositoryImpl()
 ) : ViewModel() {
+
+    private val allEmails = emailsRepository.getAllEmails()
+
+    private val allAccounts = accountsRepository.getAllUserAccounts()
 
 }
 
