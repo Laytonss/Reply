050f2d28ac6cbd476e6adae97991a6ce6f4512ad
Laytonss <jiajun.huang@thoughworks.com>
2024-04-15 14:07:57 +0800

    refactor: extract emails search bar

--- app/src/main/java/com/thoughtworks/training/reply/ui/components/EmailSearchBar.kt
+++ app/src/main/java/com/thoughtworks/training/reply/ui/components/EmailSearchBar.kt
@@ -1,11 +1,6 @@
 package com.thoughtworks.training.reply.ui.components
 
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -20,33 +15,38 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
-import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
 import com.thoughtworks.training.reply.R
 
-
 @Composable
-@Preview(showBackground = true, backgroundColor = 0xFFfdf0e3)
 @OptIn(ExperimentalMaterial3Api::class)
-fun EmailContent(@PreviewParameter(BackgroundColorProvider::class) modifier: Modifier) {
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = modifier.fillMaxWidth()
+fun EmailSearchBar() {
+    var text by rememberSaveable { mutableStateOf("") }
+    var active by rememberSaveable { mutableStateOf(false) }
+    SearchBar(
+        query = text,
+        onQueryChange = { text = it },
+        onSearch = { active = false },
+        active = active,
+        onActiveChange = {
+            active = it
+        },
+        placeholder = { Text("Search emails") },
+        leadingIcon = { Icon(Icons.Default.Search, contentDescription = null) },
+        trailingIcon = {
+            Image(
+                painter = painterResource(R.drawable.avatar_6),
+                contentDescription = "Avatar",
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(32.dp)
+                    .clip(CircleShape),
+            )
+        },
     ) {
-        EmailSearchBar()
+
     }
-}
-
-
-
-
-class BackgroundColorProvider : PreviewParameterProvider<Modifier> {
-    override val values: Sequence<Modifier>
-        get() = sequenceOf(Modifier, Modifier.fillMaxSize())
 }
